d1871ee2c6aacb2c1d39d67445eca7bae145617e
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-30 16:42:19 +0100

    add ueber generator to choose plugin type

---
+++ generators/app/index.js
@@ -0,0 +1,40 @@
+'use strict';
+var path = require('path');
+var generators = require('yeoman-generator');
+
+const knownPluginTypes = ['app', 'bundle', 'lib'];
+const defaultPluginType = ['app'];
+
+class ChooseGenerator extends generators.Base {
+
+    constructor(args, options) {
+      super(args, options);
+      // Make options available
+      this.option('skipInstall');
+    }
+
+    prompting() {
+      return this.prompt([{
+          type: 'list',
+          name: 'type',
+          message: 'Which type of plugin?',
+          choices: knownPluginTypes,
+          default: defaultPluginType
+        }]).then((props) => {
+          this.config.set('type', props.type);
+        });
+      }
+
+      default () {
+        const type = this.config.get('type');
+        this.composeWith(`phovea:${type}-plugin`, {
+          options: {
+            skipInstall: this.options.skipInstall
+          }
+        }, {
+          local: require.resolve(`../${type}-plugin`)
+        });
+      }
+    }
+
+    module.exports = ChooseGenerator;
