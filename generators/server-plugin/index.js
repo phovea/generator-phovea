1cfcaa84db611c3a8275317c424a860437e2825c
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-30 17:20:41 +0100

    start with real server plugin

---
+++ generators/server-plugin/index.js
@@ -0,0 +1,39 @@
+'use strict';
+var BasePluginGenerator = require('../../utils').Base;
+
+class PluginGenerator extends BasePluginGenerator {
+
+  constructor(args, options) {
+    super('server', args, options);
+  }
+
+  initializing() {
+    //since just last in the hierarchy used, need to do super calls
+    super.initializing();
+
+    this.config.defaults({
+      sextensions: [{
+        type: 'namespace',
+        id: 'hello_world_namespace',
+        module: 'hello_world',
+        extras: {
+          namespace: '/api/hello_world'
+        }
+      }]
+    });
+  }
+
+  prompting() {
+    return super.prompting();
+  }
+
+  default() {
+    return super.default();
+  }
+
+  writing() {
+    return super.writing();
+  }
+}
+
+module.exports = PluginGenerator;
---
+++ generators/server-plugin/templates/processed/tests/test_hello_world.py
@@ -0,0 +1,5 @@
+from <%-name%> import hello_world
+
+
+def test_hello_world():
+  assert hello_world is not None
