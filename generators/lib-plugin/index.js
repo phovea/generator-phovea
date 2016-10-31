26990b9baae6ac6c69002cf5db50b1c7b6a65ab5
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-31 11:33:04 +0100

    fix lib name

--- generators/lib-plugin/index.js
+++ generators/lib-plugin/index.js
@@ -4,7 +4,7 @@ var BasePluginGenerator = require('../../utils').Base;
 class PluginGenerator extends BasePluginGenerator {
 
   constructor(args, options) {
-    super('library', args, options);
+    super('lib', args, options);
   }
 
   initializing() {
