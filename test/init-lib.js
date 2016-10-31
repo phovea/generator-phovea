3f06d2edc86ac3fab7d52b53924bfccaf3a06206
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-31 23:50:50 +0100

    test not working

--- test/init-lib.js
+++ test/init-lib.js
@@ -3,10 +3,10 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-phovea:web-plugin', function () {
+describe('generator-phovea:init-lib', function () {
   before(function () {
-    return helpers.run(path.join(__dirname, '../generators/web-plugin'))
-      .withPrompts({someAnswer: true})
+    return helpers.run(path.join(__dirname, '../generators/init-lib'))
+      .withPrompts({ name: 'dummy'})
       .toPromise();
   });
 
