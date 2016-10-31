d020e51dcda91a583f96c41cf53d8dcd2f4e47fa
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-31 11:07:02 +0100

    fix app type name

--- generators/app-plugin/index.js
+++ generators/app-plugin/index.js
@@ -4,7 +4,7 @@ var BasePluginGenerator = require('../../utils').Base;
 class PluginGenerator extends BasePluginGenerator {
 
   constructor(args, options) {
-    super('application', args, options);
+    super('app', args, options);
   }
 
   initializing() {
