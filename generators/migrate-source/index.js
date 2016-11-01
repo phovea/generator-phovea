5d56dc1f5af830f0a4020c6e581a1f2b0344e983
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-01 17:01:44 +0100

    use scss instead of css for including style

--- generators/migrate-source/index.js
+++ generators/migrate-source/index.js
@@ -5,15 +5,15 @@ const Base = require('yeoman-generator').Base;
 function fixSourceFile(content) {
   // REMOVE /// <reference path="../../tsd.d.ts" />
   content = content.replace(/\/\/\/ <reference path="\.\.\/\.\.\/tsd\.d\.ts" \/>\s/gm, '');
-  // FROM ../caleydo_(.*)/main to phovea_$1/src/index
-  content = content.replace(/\.\.\/caleydo_(.*)\/main/gm, 'phovea_\$1/src/index');
+  // FROM /main to /index
+  content = content.replace(/\/main/gm, '/index');
   // FROM ../caleydo_(.*)/(.*)' to phovea_$1/src/$2'
   content = content.replace(/\.\.\/caleydo_(.*)\/(.*)/gm, 'phovea_\$1/src/\$2');
   // FROM import (.*) = require('(.*)') TO import * as $1 from '$2'
   content = content.replace(/import\s+(.*)\s+=\s+require\('(.*)'\)/gm, 'import * as \$1 from \'\$2\'');
 
-  // FROM /// <amd-dependency path='css!(.*)' /> TO import '$1';
-  content = content.replace(/\/\/\/ <amd-dependency path='css!(.*)' \/>/gm, 'import \'\$1.css\';');
+  // FROM /// <amd-dependency path='css!(.*)' /> TO import '$1.scss';
+  content = content.replace(/\/\/\/ <amd-dependency path='css!(.*)' \/>/gm, 'import \'\$1.scss\';');
 
 
   return content;
