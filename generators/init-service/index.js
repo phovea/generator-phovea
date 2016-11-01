bac5fdb5d42ff7149ace4e43d563705565572d72
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-01 15:56:05 +0100

    no extension for init-service

--- generators/init-service/index.js
+++ generators/init-service/index.js
@@ -1,31 +1,4 @@
 'use strict';
 var BasePluginGenerator = require('../../utils').BasePython;
 
-class PluginGenerator extends BasePluginGenerator {
-
-  initializing() {
-    // since just last in the hierarchy used, need to do super calls
-    super.initializing();
-
-    this.config.defaults({
-      sextensions: [{
-        type: 'namespace',
-        id: 'hello_world_namespace',
-        module: 'hello_world',
-        extras: {
-          namespace: '/api/hello_world'
-        }
-      }]
-    });
-  }
-
-  default() {
-    return super.default();
-  }
-
-  writing() {
-    return super.writing();
-  }
-}
-
-module.exports = PluginGenerator;
+module.exports = BasePluginGenerator;
