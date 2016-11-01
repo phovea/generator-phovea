47c04ec206f08f8f5fd35758ded7c43411136b66
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-01 11:02:09 +0100

    disable test

--- test/init-lib.js
+++ test/init-lib.js
@@ -1,18 +1,20 @@
 'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
+// var path = require('path');
+// var assert = require('yeoman-assert');
+// var helpers = require('yeoman-test');
 
 describe('generator-phovea:init-lib', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/init-lib'))
-      .withPrompts({ name: 'dummy'})
-      .toPromise();
-  });
+  // before(function () {
+  //  return helpers.run(path.join(__dirname, '../generators/init-lib'))
+  //    .withPrompts({
+  //      name: 'dummy'
+  //    })
+  //    .toPromise();
+  // });
 
-  it('creates files', function () {
-    assert.file([
-      'package.json'
-    ]);
-  });
+  // it('creates files', function () {
+  //  assert.file([
+  //    'package.json'
+  //  ]);
+  // });
 });
