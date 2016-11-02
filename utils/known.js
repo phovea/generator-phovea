ea3bfd5e981d742be827113d2da5f2467d95b095
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-02 17:58:17 +0100

    restructure libraries, no split between web and python

--- utils/known.js
+++ utils/known.js
@@ -3,33 +3,40 @@
 const Separator = require('inquirer').Separator;
 const registry = require('../knownPhoveaPlugins.json');
 
-const plugin = {};
-plugin.map = {};
-[].concat(registry.plugins, registry.splugins).forEach((p) => {
-  plugin.map[p.name] = p;
-});
-plugin.exists = (name) => name in plugin.map;
-plugin.byName = (name) => plugin.map[name];
-plugin.listWebNames = registry.plugins.map((d) => d.name);
-plugin.listServerNames = registry.splugins.map((d) => d.name);
-plugin.listNames = [].concat(
-  plugin.listWebNames,
-  new Separator(),
-  plugin.listServerNames);
+function generate(items, typesWeb, typesServer) {
+  const r = {};
 
-module.exports.plugin = plugin;
+  r.typesWeb = typesWeb;
+  r.isTypeWeb = (d) => r.typesWeb.indexOf(d.type) >= 0;
+  r.typesServer = typesServer;
+  r.isTypeServer = (d) => r.typesServer.indexOf(d.type) >= 0;
+  r.types = [].concat(
+    r.typesWeb,
+    new Separator(),
+    r.typesServer);
 
-const lib = {};
-lib.map = {};
-[].concat(registry.libraries, registry.slibraries).forEach((p) => {
-  lib.map[p.name] = p;
-});
-lib.exists = (name) => name in lib.map;
-lib.byName = (name) => lib.map[name];
-lib.listWebNames = registry.libraries.map((d) => d.name);
-lib.listServerNames = registry.slibraries.map((d) => d.name);
-lib.listNames = [].concat(
-  lib.listWebNames,
-  new Separator(),
-  lib.listServerNames);
-module.exports.lib = lib;
+  r.list = items;
+  r.listWeb = r.list.filter(r.isTypeWeb);
+  r.listServer = r.list.filter(r.isTypeServer);
+
+  r.map = {};
+  r.list.forEach((p) => {
+    r.map[p.name] = p;
+  });
+
+  r.exists = (name) => name in r.map;
+  r.byName = (name) => r.map[name];
+
+  r.listWebNames = r.listWeb.map((d) => d.name);
+  r.listServerNames = r.listServer.map((d) => d.name);
+  r.listNames = [].concat(
+    r.listWebNames,
+    new Separator(),
+    r.listServerNames);
+
+  return r;
+}
+
+module.exports.plugin = generate(registry.plugins, ['app', 'bundle', 'lib'], ['server', 'service']);
+module.exports.lib = generate(registry.libraries, ['web'], ['python']);
+
