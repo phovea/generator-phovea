ea89347f1e59da25aceb54b7e50fc5a643cdfde7
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-05 21:33:14 +0100

    fix #22 add polyfill.io in karma

--- generators/_init-web/templates/plain/karma.conf.js
+++ generators/_init-web/templates/plain/karma.conf.js
@@ -36,6 +36,11 @@ module.exports = (config) => {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
+    middleware: ['polyfill'],
+    polyfill: {
+      // features: '<feature-set>', // feature set, see polyfill-service docs for details, defaults to `{default: {}}`
+      // path: '<path>' // path to serve the polyfill script under, defaults to '/polyfill.js'
+    },
 
     browsers: [process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome'],
 
