d2a0fb9fa409efa32517e50819765da2d2f3bf39
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-13 16:53:13 +0100

    start with docker-compose (my system only)

--- generators/workspace-docker/index.js
+++ generators/workspace-docker/index.js
@@ -1,14 +1,54 @@
 'use strict';
 const Base = require('yeoman-generator').Base;
 const path = require('path');
-const generateScripts = require('../workspace-vagrant').generateScripts;
+const glob = require('glob').sync;
 const chalk = require('chalk');
+
+
+function generateScripts() {
+  const files = glob('*/requirements.txt', {
+    cwd: this.destinationPath()
+  });
+  const plugins = files.map(path.dirname);
+
+  var scripts = {};
+
+  plugins.forEach((p) => {
+    const pkg = this.fs.readJSON(this.destinationPath(p + '/package.json'));
+
+    // vagrantify commands
+    const cmds = Object.keys(pkg.scripts);
+
+    var toPatch;
+    if (cmds.includes('test:python')) { // hybrid
+      toPatch = /^(check|(test|dist|start|watch):python)$/;
+    } else { // regular server
+      toPatch = /^(check|test|dist|start|watch)$/;
+    }
+
+    // no pre post test tasks
+    cmds.filter((s) => toPatch.test(s)).forEach((s) => {
+      // generate scoped tasks
+      let cmd = `.${path.sep}withinEnv "exec 'cd ${p} && npm run ${s}'"`;
+      if (/^(start|watch)/g.test(s)) {
+        // special case for start to have the right working directory
+        let fixedscript = pkg.scripts[s].replace(/__main__\.py/, p);
+        cmd = `.${path.sep}withinEnv "${fixedscript}"`;
+      }
+      scripts[`${s}:${p}`] = cmd;
+    });
+  });
+
+  return {
+    scripts: scripts
+  };
+}
 
 class Generator extends Base {
 
   initializing() {
     this.config.defaults({
-      containerName: path.dirname(this.destinationPath())
+      containerName: 'phovea-' + path.basename(this.destinationPath())
     });
   }
 
@@ -23,7 +63,7 @@ class Generator extends Base {
   }
 
   writing() {
-    const config = this.props;
+    const config = this.config.getAll();
     const includeDot = {
       globOptions: {
         dot: true
@@ -35,9 +75,9 @@ class Generator extends Base {
     const scripts = generateScripts.call(this, '.');
     this.fs.extendJSON(this.destinationPath('package.json'), scripts);
 
-    this.log('build using:', chalk.red('docker build -t phovea_workspace .'));
-    this.log('run cli:', chalk.red('docker run -t -i -name phovea_instance phovea_workspace /bin/bash'));
-    this.log('run server:', chalk.red('docker run -t -i -name phovea_instance phovea_workspace python phovea_server'));
+    this.log('build using:', chalk.red(`docker build -t ${config.containerName} .`));
+    this.log('run cli:', chalk.red(`docker run -t -i -name ${config.containerName}_i ${config.containerName} /bin/bash`));
+    this.log('run server:', chalk.red(`docker run -t -i -name ${config.containerName}_i ${config.containerName} python phovea_server`));
   }
 }
 
