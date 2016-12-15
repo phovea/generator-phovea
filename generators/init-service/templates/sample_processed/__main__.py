cd674f6c94629383e91a69a9a2b9f7799a5ed45a
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-15 10:05:47 +0100

    add template docker file for services

--- generators/init-service/templates/sample_processed/__main__.py
+++ generators/init-service/templates/sample_processed/__main__.py
@@ -4,11 +4,24 @@
 # Licensed under the new BSD license, available at http://caleydo.org/license
 ###############################################################################
 from __future__ import print_function
-from <%-name%>.server import run, application
+import gevent.monkey
+import logging.config
 
+gevent.monkey.patch_all()  # ensure the standard libraries are patched
+
+# set configured registry
+def _get_config():
+  from phovea_server import config
+  return config.view('<%-name%>')
+
+cc = _get_config()
+# configure logging
+logging.config.dictConfig(cc.logging)
+_log = logging.getLogger(__name__)
+
+def _main():
+  from < % -name % >.server import main
+  main()
 
 if __name__ == '__main__':
-  print('run as standalone version')
-  run()
-else:
-  print('run as embedded version: %s', application)
+  _main()
