3f78f7cc629e54ff7e2e48274fabbe2734d2527b
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-10 22:26:58 +0100

    fix template

--- generators/init-slib/templates/sample_pluginname_processed/hello_world.py
+++ generators/init-slib/templates/sample_pluginname_processed/hello_world.py
@@ -5,13 +5,13 @@
 ###############################################################################
 
 from phovea_server.ns import Namespace
-from phovea_server.util import jsonfiy
+from phovea_server.util import jsonify
 import os.path
 import datetime
 import logging
 
 app = Namespace(__name__)
-_log = logger.getLogger(__name__)
+_log = logging.getLogger(__name__)
 
 @app.route('/', methods=['GET'])
 def _hello():
