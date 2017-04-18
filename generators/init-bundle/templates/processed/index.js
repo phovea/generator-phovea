ca59865014f77216b24c2667bf9f547ad62d9e6f
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-04-18 17:04:02 +0200

    add way to set and preset the public path for loading chunks in bundles see phovea/phovea_bundle_lib#20

--- generators/init-bundle/templates/processed/index.js
+++ generators/init-bundle/templates/processed/index.js
@@ -4,5 +4,24 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 
+Object.defineProperty(module.exports, '__publicPath', {
+	set: function(value) {
+		__webpack_public_path__ = value;
+	},
+	get: function() {
+		return __webpack_public_path__;
+	}
+});
+
+var parentScript = document.querySelector('script[src$="<%-name%>.js"]');
+if (parentScript) {
+  var src = parentScript.src.substring(0, parentScript.src.length - '<%-name%>.js'.length);
+  // guess and set the public path of webpack with an intelligent guess based on my own script loading tag
+  if (src !== '.') {
+    __webpack_public_path__ = src;
+  }
+}
+
+
 //request index to avoid using the pre build version
 <%- modules.map((d) => `exports.${/^phovea_.*/g.test(d) ? d.substring(7): d} = require('${d}/index');`).join('\n') %>
