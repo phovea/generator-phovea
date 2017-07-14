f14018fbc74fe1084762053bfe7446a9b2db6846
Samuel Gratzl <samuel.gratzl@jku.at>
2017-07-14 17:50:21 +0200

    interpolate index.html for enabling cache-busting

--- generators/init-app/templates/sample_plain/src/index.ts
+++ generators/init-app/templates/sample_plain/src/index.ts
@@ -2,7 +2,7 @@
  * Created by Caleydo Team on 31.08.2016.
  */
 
-import 'file-loader?name=index.html!extract-loader!html-loader!./index.html';
+import 'file-loader?name=index.html!extract-loader?interpolate!html-loader!./index.html';
 import 'file-loader?name=404.html-loader!./404.html';
 import 'file-loader?name=robots.txt!./robots.txt';
 import 'phovea_ui/src/_bootstrap';
