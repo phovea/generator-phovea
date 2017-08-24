c41cfbc9e4244d945f422e837cf9dd28cf0b6cc8
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-08-24 20:15:21 +0200

    prepare for feature flags using ifdev-loader

--- generators/_init-web/templates/phovea.tmpl.js
+++ generators/_init-web/templates/phovea.tmpl.js
@@ -6,7 +6,9 @@
 
 //register all extensions in the registry following the given pattern
 module.exports = function(registry) {
+  /// #if include('extension-type', 'extension-id')	
   //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
+  /// #endif
   // generator-phovea:begin
 <%- extensions.map((d) => `  registry.push('${d.type}', '${d.id}', function() { return import('./src/${d.module}'); }, ${stringify(d.extras, ' ')});`).join('\n\n') %>
   // generator-phovea:end
