9056452df9cf0e98a4464e8c8471a72d363ef030
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-08-31 11:22:33 +0200

    add bob search provider template

---
+++ generators/add-extension/templates/bobSearchProvider.tmpl.ts
@@ -0,0 +1,25 @@
+
+import {IResult, ISearchProvider} from 'bob/src/extensions';
+import {getTDPFilteredRows, getTDPLookup} from 'tdp_core/src/rest';
+// import './styles/idtype_color.scss';
+
+
+export default class <%-module%> implements ISearchProvider {
+  private readonly database = 'TODO';
+  private readonly table = 'TODO';
+
+  search(query: string, page: number, pageSize: number) {
+    return getTDPLookup(this.database, `${this.table}_items`, {
+      column: `name`,
+      query,
+      page: page + 1, //required to start with 1 instead of 0
+      limit: pageSize
+    });
+  }
+
+  validate(query: string[]): Promise<IResult[]> {
+    return getTDPFilteredRows(this.database, `${this.table}_items_verify`, {}, {
+      name: query
+    });
+  }
+}
