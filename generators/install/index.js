c87a5de7a36dbb0437d1200c64dd8700367dedbb
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-09-10 19:24:04 +0200

    add option to clone/install plugin

--- generators/install/index.js
+++ generators/install/index.js
@@ -1,6 +1,17 @@
 'use strict';
 const Base = require('yeoman-generator').Base;
 const path = require('path');
+const yeoman = require('yeoman-environment');
+
+function toPluginRepo(url, useSSH) {
+  const match = url.match(/(github:)?([\w\d-_]+\/)?([\w\d-_]+)(#.+)?/);
+  const repo = match ? `${match[4] ? `-b ${match[4].slice(1)} ` : ''}${useSSH ? 'git@github.com:' : 'https://github.com/'}${match[2] || ''}${match[3]}.git` : '';
+  return {
+    name: match ? match[3] : url,
+    url,
+    repo
+  }
+}
 
 class Generator extends Base {
   constructor(args, options) {
@@ -12,16 +23,52 @@ class Generator extends Base {
       required: true
     });
 
-    this.option('plugin', {
+    this.option('for', {
       type: String,
-      alias: 'p'
+      description: 'the plugin for which should the package be registered',
+      alias: 'for'
+    });
+
+    this.option('ssh', {
+      alias: 's',
+      defaults: false,
+      type: Boolean
+    });
+
+
+    this.option('type', {
+      type: String,
+      default: 'lib',
+      description: 'dependency type lib|plugin',
+      alias: 't'
     });
 
     this.plugin = null;
+    this.plugins = [];
+  }
+
+  _yo(generator, options) {
+    // call yo internally
+    const env = yeoman.createEnv([], {
+      cwd: this.cwd
+    }, this.env.adapter);
+    env.register(require.resolve('../' + generator), 'phovea:' + generator);
+    return new Promise((resolve, reject) => {
+      try {
+        this.log('running yo phovea:' + generator);
+        env.run('phovea:' + generator, options || {}, () => {
+          // wait a second after running yo to commit the files correctly
+          setTimeout(() => resolve(), 500);
+        });
+      } catch (e) {
+        console.error('error', e, e.stack);
+        reject(e);
+      }
+    });
   }
 
   default() {
-    this.plugin = this.options.plugin;
+    this.plugin = this.options.for;
 
     if (this.fs.exists(this.destinationPath('../.yo-rc-workspace.json'))) {
       // we are in a workspace
@@ -29,22 +76,46 @@ class Generator extends Base {
       this.log('switch to workspace for install dependencies but keep ' + this.plugin, 'in mind');
       process.chdir('../');
     }
-    this.log('installing: ', this.pkgs.join(' '));
-    this.npmInstall(this.pkgs, {save: true});
+    if (this.options.type.startsWith('p')) {
+      this.plugins = this.pkgs.map((url) => {
+        const p = toPluginRepo(url, this.options.ssh);
+        if (!p.repo) {
+          this.log('can resolve repository for ' + url);
+          return;
+        }
+        this.log(`cloning git clone ${p.repo}`);
+        this.spawnCommandSync('git', ['clone'].concat(p.repo.split(' ')));
+        return p;
+      });
+      this.log('updating workspace');
+      return this._yo('workspace').then(() => {
+        this.log('running npm install');
+        this.npmInstall()
+      });
+    } else {
+      this.log('installing: ', this.pkgs.join(' '));
+      this.npmInstall(this.pkgs, {save: true});
+    }
   }
 
   end() {
-    this.log(this.plugin, this.destinationPath(`package.json`));
     if (this.plugin && this.fs.exists(this.destinationPath(`package.json`))) {
       // also store the dependency in the plugin
-      const parent = this.fs.readJSON(this.destinationPath('../package.json'));
       const child = {dependencies: {}};
-      this.pkgs.forEach((p) => {
-        const pp = parent.dependencies[p];
-        if (pp) {
-          child.dependencies[p] = pp;
-        }
-      });
+      if (this.options.type.startsWith('p')) {
+        this.plugins.forEach((p) => {
+          child.dependencies[p.name] = p.url;
+        });
+      } else {
+        // copy from package json
+        const parent = this.fs.readJSON(this.destinationPath('../package.json'));
+        this.pkgs.forEach((p) => {
+          const pp = parent.dependencies[p];
+          if (pp) {
+            child.dependencies[p] = pp;
+          }
+        });
+      }
       this.fs.extendJSON(this.destinationPath(`package.json`), child);
     }
   }
