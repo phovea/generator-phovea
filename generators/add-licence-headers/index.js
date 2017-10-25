f69dea4f9665dc3f9eafc792e726ab32961f1d2b
Christian Lehner <christian.lehner@datavisyn.io>
2017-10-25 15:04:15 +0200

    read plugin folders and show them to the user

--- generators/add-licence-headers/index.js
+++ generators/add-licence-headers/index.js
@@ -27,17 +27,37 @@ class Generator extends Base {
   }
 
   prompting() {
+    const plugins = this._readAvailablePlugins();
+
     return this.prompt([
       {
         type: 'input',
         name: 'licencePath',
         message: 'Please enter the relative path to the Licence.txt file'
+      },
+      {
+        type: 'checkbox',
+        name: 'plugins',
+        message: 'Please select the plugins to add licence headers',
+        choices: plugins
       }
     ]).then((props) => {
       console.log('Props: ', props);
     });
   }
 
+  _readAvailablePlugins() {
+    const folderContents = fs.readdirSync(this.destinationPath());
+    return folderContents
+      .filter((element) => {
+        const stat = fs.statSync(this.destinationPath(element));
+        return stat.isDirectory() && // the element is a directory
+          element !== 'node_modules' && // the element is not the node_modules folder
+          !element.startsWith('_') && // the element does not start with "_" (e.g. _data, ...)
+          !element.startsWith('.') && // the element is not a hidden directory
+          fs.existsSync(this.destinationPath(element, 'phovea.js')); // the element (directory) contains a phovea.js file
+      });
+  }
 }
 
 module.exports = Generator;
