bc6041fa4b03d74548f8ccb1d7a54e0caf46411c
Christian Lehner <christian.lehner@datavisyn.io>
2017-10-25 16:13:35 +0200

    added options + refactored

--- generators/add-licence-headers/index.js
+++ generators/add-licence-headers/index.js
@@ -1,51 +1,77 @@
 'use strict';
 const fs = require('fs');
-const path = require('path');
 const Base = require('yeoman-generator').Base;
 
-const comments = {
-  ts: {
-    begin: '/*',
-    body: '*',
-    end: '*/'
-  },
-  py: {
-    begin: '#',
-    body: '#',
-    end: '#'
-  },
-  scss: {
-    begin: '/*',
-    body: '*',
-    end: '*/'
-  }
-};
+// const comments = {
+//   ts: {
+//     begin: '/*',
+//     body: '*',
+//     end: '*/'
+//   },
+//   py: {
+//     begin: '#',
+//     body: '#',
+//     end: '#'
+//   },
+//   scss: {
+//     begin: '/*',
+//     body: '*',
+//     end: '*/'
+//   }
+// };
 
 class Generator extends Base {
   constructor(args, options) {
     super(args, options);
+
+    this.option('licencePath', {
+      alias: 'l',
+      default: './licence.txt',
+      type: String,
+      desc: 'Relative path to a licence file'
+    });
+
+    this.option('plugins', {
+      alias: 'p',
+      default: '',
+      type: String,
+      desc: 'Comma separated list (without spaces) of plugins to prepend the licence to'
+    });
+  }
+
+  initializing() {
+    this.plugins = this._readAvailablePlugins();
   }
 
   prompting() {
-    const plugins = this._readAvailablePlugins();
-
     return this.prompt([
       {
         type: 'input',
         name: 'licencePath',
-        message: 'Please enter the relative path to the Licence.txt file'
+        message: 'Please enter the relative path to the Licence.txt file',
+        when: this.options.licencePath.length === 0
       },
       {
         type: 'checkbox',
         name: 'plugins',
         message: 'Please select the plugins to add licence headers',
-        choices: plugins
+        choices: this.plugins,
+        when: this.options.plugins.length === 0
       }
     ]).then((props) => {
-      console.log('Props: ', props);
-    });
+      this.licencePath = props.licencePath || this.options.licencePath;
+      this.plugins = props.plugins || this.options.plugins.split(',');
+
+      if (this.plugins.length === 0) {
+        throw new Error('No plugins given. Run the generator with the -h option to see the manual.');
+      }
+    }).catch((err) => this.log(err));
   }
 
+  /**
+   * read workspace contents and only return plugins
+   * @private
+   */
   _readAvailablePlugins() {
     const folderContents = fs.readdirSync(this.destinationPath());
     return folderContents
