20d41862cf1a2b1939744e5685d26b0e585cb000
Christian Lehner <christian.lehner@datavisyn.io>
2017-10-30 13:44:04 +0100

    add option to exclude file types and read licence file

--- generators/add-licence-headers/index.js
+++ generators/add-licence-headers/index.js
@@ -1,6 +1,12 @@
 'use strict';
 const fs = require('fs');
 const Base = require('yeoman-generator').Base;
+const chalk = require('chalk');
+
+const defaultLicenceFileName = 'licence.txt';
+const defaultLicencePath = `./${defaultLicenceFileName}`;
+
+const supportedFileTypes = ['ts', 'py', 'scss'];
 
 // const comments = {
 //   ts: {
@@ -26,9 +32,9 @@ class Generator extends Base {
 
     this.option('licencePath', {
       alias: 'l',
-      default: './licence.txt',
+      default: defaultLicencePath,
       type: String,
-      desc: 'Relative path to a licence file'
+      desc: `Relative path to a ${chalk.blue(defaultLicenceFileName)} file`
     });
 
     this.option('plugins', {
@@ -36,6 +42,13 @@ class Generator extends Base {
       default: '',
       type: String,
       desc: 'Comma separated list (without spaces) of plugins to prepend the licence to'
+    });
+
+    this.option('excludedFileTypes', {
+      alias: 'e',
+      default: '',
+      type: String,
+      desc: 'Comma separated list (without spaces) to exclude specific file types (e.g. only add to .ts files by excluding .scss and .py)'
     });
   }
 
@@ -48,8 +61,8 @@ class Generator extends Base {
       {
         type: 'input',
         name: 'licencePath',
-        message: 'Please enter the relative path to the Licence.txt file',
-        when: this.options.licencePath.length === 0
+        message: `Please enter the relative path to the ${chalk.blue(defaultLicenceFileName)} file`,
+        when: this.options.licencePath === defaultLicencePath
       },
       {
         type: 'checkbox',
@@ -57,15 +70,32 @@ class Generator extends Base {
         message: 'Please select the plugins to add licence headers',
         choices: this.plugins,
         when: this.options.plugins.length === 0
+      },
+      {
+        type: 'checkbox',
+        name: 'excludedFileTypes',
+        message: 'Exclude file types from adding headers',
+        choices: supportedFileTypes,
+        when: this.options.excludedFileTypes.length === 0
       }
     ]).then((props) => {
       this.licencePath = props.licencePath || this.options.licencePath;
       this.plugins = props.plugins || this.options.plugins.split(',');
+      this.excludedFileTypes = props.excludedFileTypes || this.options.excludedFileTypes.split(',');
 
       if (this.plugins.length === 0) {
         throw new Error('No plugins given. Run the generator with the -h option to see the manual.');
       }
+      this._readLicenceFile();
     }).catch((err) => this.log(err));
+  }
+
+  _readLicenceFile() {
+    try {
+      this.licenceText = fs.readFileSync(this.licencePath).toString();
+    } catch (e) {
+      this.log(e);
+    }
   }
 
   /**
