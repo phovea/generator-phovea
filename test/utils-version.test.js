cca94e5b3abd66bdbc96a18b8e84d88d77d976f9
oltionchampari <oltion.champari@datavisyn.io>
2019-11-07 14:03:52 +0100

    switched assert. equal() with expect() in tests
    phovea/generator-phovea#181

--- test/utils-version.test.js
+++ test/utils-version.test.js
@@ -1,104 +1,102 @@
 'use strict';
-// const path = require('path');
-const assert = require('yeoman-assert');
-// const helpers = require('yeoman-test');
+
 const version = require('../utils/version');
 
 describe('check isGitCommit()', () => {
   it('check `451709494a48af64a8a4876063c244edf41d2643` === true', () => {
-    assert.equal(version.isGitCommit('451709494a48af64a8a4876063c244edf41d2643'), true);
+    expect(version.isGitCommit('451709494a48af64a8a4876063c244edf41d2643')).toBeTruthy();
   });
 
   it('check `8747a43780e4651542facd7b4feac7bcb8e3778d` === true', () => {
-    assert.equal(version.isGitCommit('8747a43780e4651542facd7b4feac7bcb8e3778d'), true);
+    expect(version.isGitCommit('8747a43780e4651542facd7b4feac7bcb8e3778d')).toBeTruthy()
   });
 
   it('check `develop` === false', () => {
-    assert.equal(version.isGitCommit('develop'), false);
+    expect(version.isGitCommit('develop')).toBeFalsy();
   });
 
   it('check `v2.0.0` === false', () => {
-    assert.equal(version.isGitCommit('v2.0.0'), false);
+    expect(version.isGitCommit('v2.0.0')).toBeFalsy();
   });
 
   it('check `~v2.0.0` === false', () => {
-    assert.equal(version.isGitCommit('~v2.0.0'), false);
+    expect(version.isGitCommit('~v2.0.0')).toBeFalsy();
   });
 
   it('check `^v2.0.0` === false', () => {
-    assert.equal(version.isGitCommit('^v2.0.0'), false);
+    expect(version.isGitCommit('^v2.0.0')).toBeFalsy();
   });
 
   it('check `2.0.0` === false', () => {
-    assert.equal(version.isGitCommit('2.0.0'), false);
+    expect(version.isGitCommit('2.0.0')).toBeFalsy();
   });
 
   it('check `~2.0.0` === false', () => {
-    assert.equal(version.isGitCommit('~2.0.0'), false);
+    expect(version.isGitCommit('~2.0.0')).toBeFalsy();
   });
 
   it('check `^2.0.0` === false', () => {
-    assert.equal(version.isGitCommit('^2.0.0'), false);
+    expect(version.isGitCommit('^2.0.0')).toBeFalsy();
   });
 });
 
 describe('check isExactVersionTag()', () => {
   it('check `develop` === false', () => {
-    assert.equal(version.isExactVersionTag('develop'), false);
+    expect(version.isExactVersionTag('develop')).toBeFalsy();
   });
 
   it('check `v2.0.0` === true', () => {
-    assert.equal(version.isExactVersionTag('v2.0.0'), true);
+    expect(version.isExactVersionTag('v2.0.0')).toBeTruthy();
   });
 
   it('check `~v2.0.0` === false', () => {
-    assert.equal(version.isExactVersionTag('~v2.0.0'), false);
+    expect(version.isExactVersionTag('~v2.0.0')).toBeFalsy();
   });
 
   it('check `^v2.0.0` === false', () => {
-    assert.equal(version.isExactVersionTag('^v2.0.0'), false);
+    expect(version.isExactVersionTag('^v2.0.0')).toBeFalsy();
   });
 
   it('check `2.0.0` === false', () => {
-    assert.equal(version.isExactVersionTag('2.0.0'), false);
+    expect(version.isExactVersionTag('2.0.0')).toBeFalsy();
   });
 
   it('check `~2.0.0` === false', () => {
-    assert.equal(version.isExactVersionTag('~2.0.0'), false);
+    expect(version.isExactVersionTag('~2.0.0')).toBeFalsy();
   });
 
   it('check `^2.0.0` === false', () => {
-    assert.equal(version.isExactVersionTag('^2.0.0'), false);
+    expect(version.isExactVersionTag('^2.0.0')).toBeFalsy();
   });
 });
 
 describe('check isAdvancedVersionTag()', () => {
   it('check `develop` === false', () => {
-    assert.equal(version.isAdvancedVersionTag('develop'), false);
+    expect(version.isAdvancedVersionTag('develop')).toBeFalsy();
   });
 
   it('check `v2.0.0` === false', () => {
-    assert.equal(version.isAdvancedVersionTag('v2.0.0'), false);
+    expect(version.isAdvancedVersionTag('v2.0.0')).toBeFalsy();
   });
 
   it('check `~v2.0.0` === true', () => {
-    assert.equal(version.isAdvancedVersionTag('~v2.0.0'), true);
+    expect(version.isAdvancedVersionTag('~v2.0.0')).toBeTruthy();
   });
 
   it('check `^v2.0.0` === true', () => {
-    assert.equal(version.isAdvancedVersionTag('^v2.0.0'), true);
+    expect(version.isAdvancedVersionTag('^v2.0.0')).toBeTruthy();
   });
 
   it('check `2.0.0` === false', () => {
-    assert.equal(version.isAdvancedVersionTag('2.0.0'), false);
+    expect(version.isAdvancedVersionTag('2.0.0')).toBeFalsy();
   });
 
   it('check `~2.0.0` === false', () => {
-    assert.equal(version.isAdvancedVersionTag('~2.0.0'), false);
+    expect(version.isAdvancedVersionTag('~2.0.0')).toBeFalsy();
   });
 
   it('check `^2.0.0` === false', () => {
-    assert.equal(version.isAdvancedVersionTag('^2.0.0'), false);
+    expect(version.isAdvancedVersionTag('^2.0.0')).toBeFalsy();
   });
 });
 
@@ -118,20 +116,20 @@ describe('transform git log to version tags list', () => {
   const versionTags = version.extractVersionsFromGitLog(gitLog);
 
   it('check number of extracted versions', () => {
-    assert.equal(versionTags.length, 7);
+    expect(versionTags.length).toBe(7);
   });
 
   it('check if tags without `v` are removed', () => {
-    assert.equal(versionTags.indexOf('caleydo_web'), -1);
+    expect(versionTags.indexOf('caleydo_web')).toBe(-1);
   });
 
   it('check if tags ending with `^{}` are removed', () => {
-    assert.equal(versionTags.indexOf('v1.0.0^{}'), -1);
-    assert.equal(versionTags.filter((v) => v === 'v1.0.0').length, 1);
+    expect(versionTags.indexOf('v1.0.0^{}')).toBe(-1);
+    expect(versionTags.filter((v) => v === 'v1.0.0').length).toBe(1);
   });
 
   it('find specific version tag in list', () => {
-    assert.equal(versionTags.indexOf('v0.0.5'), 0);
+    expect(versionTags.indexOf('v0.0.5')).toBe(0);
   });
 });
 
@@ -140,34 +138,34 @@ describe('check semver.satisfies', () => {
 
   it('check against target version with caret operator', () => {
     const targetVersion = '^v2.0.0';
-    assert.equal(semver.satisfies('v1.0.0', targetVersion), false);
-    assert.equal(semver.satisfies('v2.0.0', targetVersion), true);
-    assert.equal(semver.satisfies('v2.0.1', targetVersion), true);
-    assert.equal(semver.satisfies('v2.0.2', targetVersion), true);
-    assert.equal(semver.satisfies('v2.1.0', targetVersion), true);
-    assert.equal(semver.satisfies('v2.1.1', targetVersion), true);
-    assert.equal(semver.satisfies('v2.1.2', targetVersion), true);
-    assert.equal(semver.satisfies('v2.2.0', targetVersion), true);
-    assert.equal(semver.satisfies('v2.2.1', targetVersion), true);
-    assert.equal(semver.satisfies('v2.2.2', targetVersion), true);
-    assert.equal(semver.satisfies('v3.0.0', targetVersion), false);
-    assert.equal(semver.satisfies('v3.1.0', targetVersion), false);
+    expect(semver.satisfies('v1.0.0', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.0.0', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.0.1', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.0.2', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.1.0', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.1.1', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.1.2', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.2.0', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.2.1', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.2.2', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v3.0.0', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v3.1.0', targetVersion)).toBeFalsy();
   });
 
   it('check target version with tilde operator', () => {
     const targetVersion = '~v2.0.0';
-    assert.equal(semver.satisfies('v1.0.0', targetVersion), false);
-    assert.equal(semver.satisfies('v2.0.0', targetVersion), true);
-    assert.equal(semver.satisfies('v2.0.1', targetVersion), true);
-    assert.equal(semver.satisfies('v2.0.2', targetVersion), true);
-    assert.equal(semver.satisfies('v2.1.0', targetVersion), false);
-    assert.equal(semver.satisfies('v2.1.1', targetVersion), false);
-    assert.equal(semver.satisfies('v2.1.2', targetVersion), false);
-    assert.equal(semver.satisfies('v2.2.0', targetVersion), false);
-    assert.equal(semver.satisfies('v2.2.1', targetVersion), false);
-    assert.equal(semver.satisfies('v2.2.2', targetVersion), false);
-    assert.equal(semver.satisfies('v3.0.0', targetVersion), false);
-    assert.equal(semver.satisfies('v3.1.0', targetVersion), false);
+    expect(semver.satisfies('v1.0.0', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.0.0', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.0.1', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.0.2', targetVersion)).toBeTruthy();
+    expect(semver.satisfies('v2.1.0', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.1.1', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.1.2', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.2.0', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.2.1', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v2.2.2', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v3.0.0', targetVersion)).toBeFalsy();
+    expect(semver.satisfies('v3.1.0', targetVersion)).toBeFalsy();
   });
 });
 
@@ -191,26 +189,26 @@ describe('find highest version from list', () => {
 
   it('find exact version `v2.0.0`', () => {
     const targetVersion = 'v2.0.0';
-    assert.equal(version.findHighestVersion(sourceVersions, targetVersion), 'v2.0.0');
+    expect(version.findHighestVersion(sourceVersions, targetVersion)).toBe('v2.0.0');
   });
 
   it('find patch version `~v2.0.0`', () => {
     const targetVersion = '~v2.0.0';
-    assert.equal(version.findHighestVersion(sourceVersions, targetVersion), 'v2.0.2');
+    expect(version.findHighestVersion(sourceVersions, targetVersion)).toBe('v2.0.2');
   });
 
   it('find minor version `^v2.0.0`', () => {
     const targetVersion = '^v2.0.0';
-    assert.equal(version.findHighestVersion(sourceVersions, targetVersion), 'v2.2.2');
+    expect(version.findHighestVersion(sourceVersions, targetVersion)).toBe('v2.2.2');
   });
 
   it('find non-existing `v4.0.0`', () => {
     const targetVersion = 'v4.0.0';
-    assert.equal(version.findHighestVersion(sourceVersions, targetVersion), undefined);
+    expect(version.findHighestVersion(sourceVersions, targetVersion)).toBeUndefined();
   });
 
   it('find non-existing `^v3.2.0`', () => {
     const targetVersion = '^v3.2.0';
-    assert.equal(version.findHighestVersion(sourceVersions, targetVersion), undefined);
+    expect(version.findHighestVersion(sourceVersions, targetVersion)).toBeUndefined();
   });
 });
