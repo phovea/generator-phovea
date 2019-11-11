22495710e6bf6994c0607e61fe09c6acacbaa8f2
oltionchampari <oltion.champari@datavisyn.io>
2019-11-11 13:31:55 +0100

    deleted model test
    phovea/generator-phovea#181

--- test/init-lib.test.js
+++ test/init-lib.test.js
@@ -1,34 +1,22 @@
-// 'use-strict';
 
-// const rimraf = require('rimraf');
-// const path = require('path');
-
-// const helpers = require('yeoman-test');
-// const assert = require('yeoman-assert');
-
-
-// describe('generator-phovea:init-lib', () => {
-
-//   beforeEach(() => {
-//     return helpers.run(path.join(__dirname, '../generators/test'))
-//       .inDir(path.join(__dirname, 'tmp'))
-//       .withPrompts({})
-//   });
-
-//   afterEach(() => {
-//     rimraf.sync(path.join(__dirname, 'tmp'));
-//   });
-
-//   it('creates an index.html file in the target folder', () => {
-//     assert.file(path.join(__dirname,
-//       'tmp/public/index.html'
-//     ));
-//   })
-
-//   it('index.html folder has the right title', () => {
-//     assert.fileContent(path.join(__dirname,
-//       'tmp/public/index.html'
-//     ), 'Templating with Yeoman');
-//   })
-
-// });
+'use strict';
+// var path = require('path');
+// var assert = require('yeoman-assert');
+// var helpers = require('yeoman-test');
+describe('generator-phovea:init-lib', function () {
+  // before(function () {
+  //  return helpers.run(path.join(__dirname, '../generators/init-lib'))
+  //    .withPrompts({
+  //      name: 'dummy'
+  //    })
+  //    .toPromise();
+  // });
+  // it('creates files', function () {
+  //  assert.file([
+  //    'package.json'
+  //  ]);
+  // });
+  it('runs', function () {
+    expect(true).not.toBe(false);
+  });
+});
