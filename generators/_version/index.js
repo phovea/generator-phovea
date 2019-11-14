07859637662e21db68f9a7c42c75c83bceb896c6
Holger Stitz <holger.stitz@datavisyn.io>
2019-11-14 13:21:42 +0100

    Add comment to `hasBeenNotified` flag

--- generators/_version/index.js
+++ generators/_version/index.js
@@ -3,6 +3,11 @@ const pkg = require('../../package.json');
 const Generator = require('yeoman-generator');
 const updateNotifier = require('update-notifier');
 
+/**
+ * This flag avoids to show the update notification again,
+ * in case this version check generator is called multiple times
+ * within the same yeoman command.
+ */
 let hasBeenNotified = false;
 
 function runUpdateNotifier() {
@@ -12,7 +17,7 @@ function runUpdateNotifier() {
   hasBeenNotified = true;
   const notifier = updateNotifier({
     pkg,
-    updateCheckInterval: 1000 * 60 * 60 * 24 * 2// 2 days
+    updateCheckInterval: 1000 * 60 * 60 * 24 * 2 // 2 days
   });
 
   notifier.notify({isGlobal: true});
