49de0d3a12ed99246218f8ccc04cfc33e0a09717
thinkh <holger.stitz@datavisyn.io>
2020-04-17 13:20:34 +0200

    Remove unecessary space

--- generators/_init-web/templates/phovea.tmpl.ts
+++ generators/_init-web/templates/phovea.tmpl.ts
@@ -4,7 +4,7 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 
- import {IRegistry} from 'phovea_core/src/plugin';
+import {IRegistry} from 'phovea_core/src/plugin';
 
 export default function (registry: IRegistry) {
   /// #if include('extension-type', 'extension-id')
