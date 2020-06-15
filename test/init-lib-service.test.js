c6affcd76fb758be8b9efc966e902dc6f4f2e9ce
oltionchampari <oltion.champari@datavisyn.io>
2020-06-15 09:46:17 +0200

    Add basic test for `init-lib-service` subgenerator
    phovea/generator-phovea#380

---
+++ test/init-lib-service.test.js
@@ -0,0 +1,48 @@
+
+'use strict';
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const rimraf = require('rimraf');
+const fse = require('fs-extra');
+const testUtils=require('./testUtils');
+/**
+ * Directory name to run the generator
+ */
+const target = '../libservice';
+
+/**
+ * Subgenerators composed with the `init-lib-service` subgenerator.
+ */
+const GENERATOR_DEPENDENCIES = [
+  '../generators/_node',
+  '../generators/_init-hybrid',
+  '../generators/init-lib',
+  '../generators/_init-web',
+  '../generators/init-service',
+  '../generators/_init-python',
+  '../generators/_check-own-version',
+  '../generators/check-node-version',
+];
+
+
+
+describe('generate lib-service plugin with default prompt values', () => {
+
+
+  beforeAll(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/init-lib-service'))
+      .inDir(path.join(__dirname, target), () => null)
+      .withGenerators(GENERATOR_DEPENDENCIES);
+  });
+
+  afterAll(() => {
+    rimraf.sync(path.join(__dirname, target));
+  });
+
+  it('generates `package.json` with the correct devDependencies', () => {
+    const initWebPackage = fse.readJSONSync(testUtils.templatePath('_init-web', 'package.tmpl.json'));
+    assert.jsonFileContent('package.json', {devDependencies: initWebPackage.devDependencies});
+  });
+});
