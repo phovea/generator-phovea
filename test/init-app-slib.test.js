4b06b2e899e764752c9b108fbee9ff44cc1435d5
oltionchampari <oltion.champari@datavisyn.io>
2020-06-15 09:38:19 +0200

    Add basic test for the `init-app-slib` subgenerator
    phovea/generator-phovea#380

---
+++ test/init-app-slib.test.js
@@ -0,0 +1,51 @@
+
+'use strict';
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const rimraf = require('rimraf');
+const fse = require('fs-extra');
+const testUtils=require('./testUtils');
+/**
+ * Directory name to run the generator
+ */
+const target = '../appslib';
+
+/**
+ * Subgenerators composed with the `init-app-slib` subgenerator.
+ */
+const GENERATOR_DEPENDENCIES = [
+  '../generators/_node',
+  '../generators/_init-hybrid',
+  '../generators/init-app',
+  '../generators/_init-web',
+  '../generators/init-slib',
+  '../generators/_init-python',
+  '../generators/_check-own-version',
+  '../generators/check-node-version',
+];
+
+
+
+describe('generate app-slib plugin with prompt `app: appName` and the rest default prompt values', () => {
+
+
+  beforeAll(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/init-app-slib'))
+      .inDir(path.join(__dirname, target), () => null)
+      .withPrompts({
+        app:'appName'
+    })
+      .withGenerators(GENERATOR_DEPENDENCIES);
+  });
+
+  afterAll(() => {
+    rimraf.sync(path.join(__dirname, target));
+  });
+
+  it('generates `package.json` with the correct devDependencies', () => {
+    const initWebPackage = fse.readJSONSync(testUtils.templatePath('_init-web', 'package.tmpl.json'));
+    assert.jsonFileContent('package.json', {devDependencies: initWebPackage.devDependencies});
+  });
+});
