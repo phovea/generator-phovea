3dbe833b81c18cffc56e9550768b436f2da9aff6
oltionchampari <oltion.champari@datavisyn.io>
2020-06-15 09:33:04 +0200

    Add basic test for the`init-app` subgenerator
    phovea/generator-phovea#380

---
+++ test/init-app.test.js
@@ -0,0 +1,47 @@
+
+'use strict';
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const rimraf = require('rimraf');
+const fse = require('fs-extra');
+const testUtils = require('./testUtils');
+
+/**
+ * Directory name to run the generator
+ */
+const target = '../app';
+
+/**
+ * Subgenerators composed with the `init-app` subgenerator.
+ */
+const GENERATOR_DEPENDENCIES = [
+  '../generators/_node',
+  '../generators/init-lib',
+  '../generators/_init-web',
+  '../generators/_check-own-version',
+  '../generators/check-node-version',
+];
+
+describe('generate app plugin with prompt `app: appName` and the rest default prompt values', () => {
+
+
+  beforeAll(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/init-app'))
+      .inDir(path.join(__dirname, target), () => null)
+      .withPrompts({
+          app:'appName'
+      })
+      .withGenerators(GENERATOR_DEPENDENCIES);
+  });
+
+  afterAll(() => {
+    rimraf.sync(path.join(__dirname, target));
+  });
+
+  it('generates `package.json` with correct devDependencies', () => {
+    const initWebPackage = fse.readJSONSync(testUtils.templatePath('_init-web', 'package.tmpl.json'));
+    assert.jsonFileContent('package.json', {devDependencies: initWebPackage.devDependencies});
+  });
+});
