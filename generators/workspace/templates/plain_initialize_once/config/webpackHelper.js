cb4793185fab8f900fa0e79a3a2aa71918fa1582
Holger Stitz <holger.stitz@datavisyn.io>
2020-12-28 11:39:28 +0100

    Refactor workspace and default app path
    #450

--- generators/workspace/templates/plain_initialize_once/config/webpackHelper.js
+++ generators/workspace/templates/plain_initialize_once/config/webpackHelper.js
@@ -6,18 +6,16 @@ const fs = require('fs');
  * @param entry
  * @returns {*}
  */
-function injectRegistry(defaultApp, extraFiles, entry) {
+function injectRegistry(workspacePath, defaultAppPath, extraFiles, entry) {
   // build also the registry
   if (typeof entry === 'string') {
       return extraFiles.concat(entry);
   }
   const transformed = {};
   Object.keys(entry).forEach((key) => {
-    const workspacePath = path.join(__dirname, `..`);
-    const defaultAppPath = path.join(workspacePath, defaultApp);
     const workspaceSCSS = entry[key]['scss'] ? path.join(defaultAppPath, entry[key]['scss']) : path.join(workspacePath, `workspace.scss`);
     const entryJS = path.join(defaultAppPath, entry[key]['js']);
-    
+
     transformed[key] = extraFiles.concat(workspaceSCSS, entryJS);
   });
   return transformed;
